015c1f158731f071a3f3a69ea967c80128bc8d46
christian-brindley <christian.brindley@gmail.com>
2020-06-18 16:21:42 +0100

    Update dsmetrics.sh

--- scripts/dsmetrics.sh
+++ scripts/dsmetrics.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#
-# Config
-#
-
-MONITOR_USERNAME="monitor"
-MONITOR_PASSWORD="Passw0rd"
-DS_URL="https://userstore-1:18443"
-DEFAULT_METRICS="\
-disk./.free_space \
-disk./.free_space_low_threshold \
-disk./.free_space_full_threshold"
-
 # getMetrics dsurl userid password [fields]
 # 
 # Get DS metrics
@@ -19,53 +7,64 @@ disk./.free_space_full_threshold"
 
 function getMetrics() {
     dsurl=$1
-    filter=$2
-    userid=$3
-    password=$4
-    fields=$5
+    userid=$2
+    password=$3
+    metrics=$4
 
-    json=$(curl -k -s --user "$userid:$password"  "${dsurl}/metrics/api?_queryFilter=$filter")
-
-    echo $json
-}
-
-function usage () {
-  echo "Usage: dsmetrics.sh { fields | -a | -d }"
-}
-
-# Go
-
-if [ $# == 0 ]
-then
-    usage
-    exit 1
-fi
-
-
-if [ "$1" == "-a" ]
-then
-    filter=true
-else
     filter=""
-    if [ $1 == "-d" ]
-    then
-        metrics="$DEFAULT_METRICS"
-    else
-        metrics="$@"
-    fi
-    echo Metrics: $metrics
 
     for metric in $metrics
     do
-	    echo $metric
         if [ -n "$filter" ]
 	then
 	    filter="${filter}%20or%20"
 	fi
         filter="${filter}_id%20eq%20%22${metric}%22"
     done
+
+    json=$(curl -k -s --user "$userid:$password"  "${dsurl}/metrics/api?_queryFilter=$filter")
+
+    echo $json
+}
+
+
+# getPrometheusMetrics dsurl userid password [fields]
+# 
+# Get DS metrics via Prometheus endpoint
+#
+
+function getPrometheusMetrics() {
+    dsurl=$1
+    userid=$2
+    password=$3
+    metrics=$4
+
+    filter=$( echo $metrics | sed "s/\ /\\\|/g" )
+    curl -k -s --user "$userid:$password"  "${dsurl}/metrics/prometheus" | grep -v "^#" | grep $filter
+}
+
+
+function usage () {
+    echo "Usage: dsmetrics.sh propertiesfile"
+}
+
+# Go
+
+if [ $# != 1 ]
+then
+    usage
+    exit 1
 fi
 
-metrics=$(getMetrics "$DS_URL" $filter "$MONITOR_USERNAME" "$MONITOR_PASSWORD") 
+propertiesfile=$1
+
+. $propertiesfile
+
+if [ "$METHOD" == "prometheus" ]
+then
+    metrics=$( getPrometheusMetrics "$DS_BASE_URL" "$MONITOR_USERNAME" "$MONITOR_PASSWORD" "$PROM_METRICS" )
+else
+    metrics=$( getMetrics "$DS_BASE_URL" "$MONITOR_USERNAME" "$MONITOR_PASSWORD" "$API_METRICS" ) 
+fi
 
 echo $metrics
