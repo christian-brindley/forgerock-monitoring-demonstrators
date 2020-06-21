405a70ae4c3ebb94532f51328dfe33d56ca707bc
christian-brindley <christian.brindley@gmail.com>
2020-06-21 10:38:21 +0100

    Update dsmetrics.sh

--- scripts/dsmetrics/dsmetrics.sh
+++ scripts/dsmetrics/dsmetrics.sh
@@ -1,9 +1,32 @@
 #!/bin/bash 
 
+##############################################################################
+# DISCLAIMER                                                                 #
+#                                                                            #
+# The sample code described herein is provided on an "as is" basis, without  #
+# warranty of any kind, to the fullest extent permitted by law. ForgeRock    #
+# does not warrant or guarantee the individual success developers may have   #
+# in implementing the sample code on their development platforms or in       #
+# production configurations. ForgeRock does not warrant, guarantee or make   #
+# any representations regarding the use, results of use, accuracy,           #
+# timeliness or completeness of any data or information relating to the      #
+# sample code. ForgeRock disclaims all warranties, expressed or implied, and #
+# in particular, disclaims all warranties of merchantability, and            #
+# warranties related to the code, or any service or software related         #
+# thereto.                                                                   #
+#                                                                            #
+# ForgeRock shall not be liable for any direct, indirect or consequential    #
+# damages or costs of any type arising out of any action taken by you or     #
+# others related to the sample code.                                         #
+##############################################################################
+
+
+
 # getMetrics dsurl userid password [fields]
 # 
 # Get DS metrics
 #
+
 
 function getMetrics() {
     dsurl=$1
@@ -176,4 +199,3 @@ fi
 
 metrics=$( echo "$metrics" | sed ':a;N;$!ba;s/\n/ /g' )
 log "$metrics"
-
